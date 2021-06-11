88a05deff0e9a21ad413fdd7fb5cb7d53772526a
Felipe Eires <felipe.eires@cheftime.com.br>
2021-06-11 19:10:54 -0300

    Endpoint para buscar os todos os jogos

--- src/server.js
+++ src/server.js
@@ -1,11 +1,64 @@
-const express = require('express');
 
+const express = require('express');
+const ParserFileData = require('./parser/ParserFileData');
+const fs = require('fs');
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ content: "Hello World" });
+app.get('/games', (req, res) => {
+  fs.readFile('./log/games.log', (error, file) => {
+    if(error) {
+      console.log(error);
+      res.status(500).send({ message : error });
+    }
+
+    let textFile = file.toString();
+    let data = new ParserFileData();
+    let result = data.parse(textFile);
+
+    res.json(result);
+  });
 });
 
 app.listen(3001, () => {
-  console.log('Backend Online.')
+  console.log('Server online!');
 });
+
+// const express = require('express');
+// const app = express();
+// const fs = require('fs');
+// const ParserFileData = require('./parser/ParserFileData');
+
+// app.get('/games', (req, res) => {
+//   fs.readFile('./log/games.log', (error, file) => {
+//     if(error) {
+//       console.log(error);
+//       res.status(500).send({ message : error });
+//     }
+
+//     let textFile = file.toString();
+//     let data = new ParserFileData();
+//     let result = data.parse(textFile);
+
+//     res.json(result);
+//   });
+// });
+
+// app.get('/games/:id', (req, res) => {
+//   fs.readFile('./log/games.log', (error, file) => {
+//     if(error) {
+//       console.log(error);
+//       res.status(500).send({ message : error });
+//     }
+
+//     let textFile = file.toString();
+//     let data = new ParserFileData().parse(textFile);
+//     const result = data[`game_${req.params.id}`]
+
+//     res.json(result);
+//   });
+// });
+
+// app.listen(3001, () => {
+//   console.log('Server online!');
+// });
+
