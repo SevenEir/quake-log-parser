b953cbc7db35734e83f6f809a434f305f9a03c53
Felipe Eires <felipe.eires@cheftime.com.br>
2021-06-11 19:12:11 -0300

    Endpoint para buscar dados de um jogo em especifico através do id

--- src/server.js
+++ src/server.js
@@ -1,8 +1,7 @@
-
 const express = require('express');
-const ParserFileData = require('./parser/ParserFileData');
-const fs = require('fs');
 const app = express();
+const fs = require('fs');
+const ParserFileData = require('./parser/ParserFileData');
 
 app.get('/games', (req, res) => {
   fs.readFile('./log/games.log', (error, file) => {
@@ -19,46 +18,22 @@ app.get('/games', (req, res) => {
   });
 });
 
+app.get('/games/:id', (req, res) => {
+  fs.readFile('./log/games.log', (error, file) => {
+    if(error) {
+      console.log(error);
+      res.status(500).send({ message : error });
+    }
+
+    let textFile = file.toString();
+    let data = new ParserFileData().parse(textFile);
+    const result = data[`game_${req.params.id}`]
+
+    res.json(result);
+  });
+});
+
 app.listen(3001, () => {
   console.log('Server online!');
 });
-
-// const express = require('express');
-// const app = express();
-// const fs = require('fs');
-// const ParserFileData = require('./parser/ParserFileData');
-
-// app.get('/games', (req, res) => {
-//   fs.readFile('./log/games.log', (error, file) => {
-//     if(error) {
-//       console.log(error);
-//       res.status(500).send({ message : error });
-//     }
-
-//     let textFile = file.toString();
-//     let data = new ParserFileData();
-//     let result = data.parse(textFile);
-
-//     res.json(result);
-//   });
-// });
-
-// app.get('/games/:id', (req, res) => {
-//   fs.readFile('./log/games.log', (error, file) => {
-//     if(error) {
-//       console.log(error);
-//       res.status(500).send({ message : error });
-//     }
-
-//     let textFile = file.toString();
-//     let data = new ParserFileData().parse(textFile);
-//     const result = data[`game_${req.params.id}`]
-
-//     res.json(result);
-//   });
-// });
-
-// app.listen(3001, () => {
-//   console.log('Server online!');
-// });
 
